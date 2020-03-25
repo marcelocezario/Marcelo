8777a7efcaedd07f877310f693aed53203d9bc1d
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-03-25 18:35:31 -0300

    Criando um crud completo

---
+++ header.php
@@ -0,0 +1,14 @@
+ <!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Curso PHP & MySQL Udemy</title>
+  </head>
+  
+  <body>
+    <header>
+        <img src="" tittle="Logo" alt="Logo">
+        <div id="menu"></div>
+    </header>
+
+    <div id="conteudo">
