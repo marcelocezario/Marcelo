aea5db6cd2c769a007c31ef4b0e58ba6025d8653
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-03-25 21:13:24 -0300

    Organizando Index.php

--- index.php
+++ index.php
@@ -7,33 +7,33 @@ include 'db.php';
 include 'header.php';
 
 # Conteúdo da página
-if(isset($_GET['pagina'])){
+if (isset($_GET['pagina'])) {
     $pagina = $_GET['pagina'];
-}
-else{
+} else {
     $pagina = 'home';
 }
-
-if($pagina == 'cursos'){
-    include 'views/cursos.php';
-}
-elseif($pagina == 'inserir_curso'){
-    include 'views/inserir_curso.php';
-}
-elseif($pagina == 'alunos'){
-    include 'views/alunos.php';
-}
-elseif($pagina == 'inserir_aluno'){
-    include 'views/inserir_aluno.php';
-}
-elseif($pagina == 'matriculas'){
-    include 'views/matriculas.php';
-}
-elseif($pagina == 'inserir_matricula'){
-    include 'views/inserir_matricula.php';
-}
-else {
-    include 'views/home.php';
+switch ($pagina) {
+    case 'alunos':
+        include 'views/alunos.php';
+        break;
+    case 'inserir_aluno':
+        include 'views/inserir_aluno.php';
+        break;
+    case 'cursos':
+        include 'views/cursos.php';
+        break;
+    case 'inserir_curso':
+        include 'views/inserir_curso.php';
+        break;
+    case 'matricula':
+        include 'views/matriculas.php';
+        break;
+    case 'inserir_matricula':
+        include 'views/inserir_matricula.php';
+        break;
+    default:
+        include 'views/home.php';
+        break;
 }
 
 # Rodapé
