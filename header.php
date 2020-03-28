f85ad8d8dbbe7ae61ac22fa4ee9ce5369ce91bc0
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-03-28 15:27:09 -0300

    Adicionando link para uso do Font Awesome

--- header.php
+++ header.php
@@ -7,17 +7,18 @@
      <link rel="stylesheet" href="css/estilo.css">
      <link rel="stylesheet" href="css/bootstrap.min.css">
      <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
  </head>
 
  <body>
      <header>
          <div class="container">
              <a href="?pagina=home"><img src="img/logo.png" id="logo" tittle="Logo" alt="Logo">
-             <div id="menu">
-                 <a href="?pagina=cursos">Cursos</a>
-                 <a href="?pagina=alunos">Alunos</a>
-                 <a href="?pagina=matriculas">Matrículas</a>
-             </div>
+                 <div id="menu">
+                     <a href="?pagina=cursos">Cursos</a>
+                     <a href="?pagina=alunos">Alunos</a>
+                     <a href="?pagina=matriculas">Matrículas</a>
+                 </div>
          </div>
      </header>
 
