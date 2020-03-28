3a7c45a9403c17b587d4bc4bfe2f8f262cf81664
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-03-28 15:34:43 -0300

    Utilizando ícones do font awesome

--- views/alunos.php
+++ views/alunos.php
@@ -1,6 +1,6 @@
 <a href="?pagina=inserir_aluno">Inserir novo aluno</a>
 
-<table class="table" id="alunos">
+<table class="table table-hover table-striped" id="alunos">
     <thead>
         <tr>
             <th>Nome aluno</th>
@@ -17,8 +17,12 @@
             echo '<td>' . $linha['nome_aluno'] . '</td>';
             echo '<td>' . $linha['data_nascimento'] . '</td>';
         ?>
-            <td><a href="?pagina=inserir_aluno&editar=<?php echo $linha['id_aluno']; ?>">Editar</a></td>
-            <td><a href="deleta_aluno.php?id_aluno=<?php echo $linha['id_aluno']; ?>">Deletar</a></td>
+            <td><a href="?pagina=inserir_aluno&editar=<?php echo $linha['id_aluno']; ?>">
+                    <i class="far fa-edit"></i>
+                </a></td>
+            <td><a href="deleta_aluno.php?id_aluno=<?php echo $linha['id_aluno']; ?>">
+                    <i class="far fa-trash-alt"></i>
+                </a></td>
             </tr>
 
         <?php
